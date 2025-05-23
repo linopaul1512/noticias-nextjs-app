a8ea4f18c1ac0a0122928c832abafcc5ffe24da6
linopaul_1512 <linobenavidesgabaldon.com>
2025-05-23 09:23:16 -0400

    ejemplo de login

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,15 +1,11 @@
-// pages/api/auth/login.js
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { correo, contrasena } = req.body;
 
-    // Aquí deberías verificar las credenciales con tu base de datos
-    // Este es un ejemplo simplificado
+   
     if (correo === 'usuario@example.com' && contrasena === 'contraseña123') {
-      // Autenticación exitosa
       res.status(200).json({ mensaje: 'Inicio de sesión exitoso' });
     } else {
-      // Credenciales inválidas
       res.status(401).json({ mensaje: 'Credenciales inválidas' });
     }
   } else {
