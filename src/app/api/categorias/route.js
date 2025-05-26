6969f25bb5918d5b97aeb5a9b5b7de7f3be11acd
arisinnato <sinnatoarianna@gmail.com>
2025-05-26 16:49:54 -0400

    cambios n paqutes

--- src/app/api/categorias/route.js
+++ src/app/api/categorias/route.js
@@ -3,14 +3,31 @@ import  connectDB  from "@/app/libs/mongoDB";
 import Categoria from '@/app/models/categoria';
 
 
+
 export async function POST(request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: 'No autenticado' }, { status: 401 });
+  }
+
+  if (session.user.Tipo !== 'autor') {
+    return NextResponse.json({ error: 'No autorizado. Solo los autores pueden crear categorias.' }, { status: 403 });
+  }
+
   try {
     await connectDB();
     const data = await request.json();
-    const nuevaCategoria = new Categoria(data);
+
+    const nuevaCategoria = new Noticia({
+      ...data,
+      autor: session.user.correo, 
+    });
+
     await nuevaCategoria.save();
     return NextResponse.json(nuevaCategoria, { status: 201 });
   } catch (error) {
+    console.error('Error al crear la categoría:', error);
     return NextResponse.json({ error: 'Error al crear la categoría' }, { status: 500 });
   }
 }
