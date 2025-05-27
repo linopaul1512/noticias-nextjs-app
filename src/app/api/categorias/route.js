0d76863387cac46149e43127a90149551244456f
linopaul_1512 <linobenavidesgabaldon.com>
2025-05-27 10:15:58 -0400

    categoria

--- src/app/api/categorias/route.js
+++ src/app/api/categorias/route.js
@@ -3,32 +3,19 @@ import  connectDB  from "@/app/libs/mongoDB";
 import Categoria from '@/app/models/categoria';
 
 
+
 export async function POST(request) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ error: 'No autenticado' }, { status: 401 });
-  }
-
-  if (session.user.Tipo !== 'autor') {
-    return NextResponse.json({ error: 'No autorizado. Solo los autores pueden crear categorias.' }, { status: 403 });
-  }
-
   try {
     await connectDB();
     const data = await request.json();
-
-    const nuevaCategoria = new Noticia({
-      ...data,
-      autor: session.user.correo, 
-    });
-
+    const nuevaCategoria = new Categoria(data);
     await nuevaCategoria.save();
     return NextResponse.json(nuevaCategoria, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: 'Error al crear la categoría' }, { status: 500 });
+    return NextResponse.json({ error: 'Error al crear categoría' }, { status: 500 });
   }
 }
+
 
 /*
 export async function POST(request) {
