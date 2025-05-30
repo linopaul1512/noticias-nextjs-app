530b0ff1c1951c8de93710423b0016f788946873
arisinnato <sinnatoarianna@gmail.com>
2025-05-30 15:32:04 -0400

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
-  title: "Noticias App",
-  description: "Autenticación con Next.js",
+  title: 'Noticias App',
+  description: 'Plataforma de noticias con Next.js',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`container mt-4 ${inter.className}`}>
-        {children}
-      </body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          integrity="sha384..."
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
+
 
